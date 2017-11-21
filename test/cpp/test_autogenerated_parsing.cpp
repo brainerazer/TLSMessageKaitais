a9fc91d078ab1ae70f613fa414627d81f4d1f455
Bohdan Rybak <bohdan.rybak@ring.com>
2017-11-21 16:19:12 +0200

    Integration test

--- test/cpp/test_autogenerated_parsing.cpp
+++ test/cpp/test_autogenerated_parsing.cpp
@@ -2,11 +2,12 @@
 
 #include <kaitai/kaitaistream.h>
 #include <autogen/tls_record.h>
+#include <autogen/tls_handshake_record.h>
 
 #include <fstream>
 #include <iostream>
 
-TEST_CASE("Files are parsed correctly") {
+TEST_CASE("Entities are parsed correctly on their own") {
     SECTION("Record is parsed correctly") {
         std::ifstream ifs("../sample_data/tls_record_handshake_client_hello.bin", std::ifstream::binary);
         kaitai::kstream ks(&ifs);
@@ -19,5 +20,33 @@ TEST_CASE("Files are parsed correctly") {
         REQUIRE(rec.payload()[0] == 1);
         REQUIRE(rec.payload().size() == rec.len());
         REQUIRE(rec.payload()[rec.payload().size() - 1] == 3);
+    }
+
+    SECTION("Handshake is parsed correctly") {
+        std::ifstream ifs("../sample_data/tls_handshake_client_hello.bin", std::ifstream::binary);
+        kaitai::kstream ks(&ifs);
+        tls_handshake_record_t hs = tls_handshake_record_t(&ks);
+
+        REQUIRE(hs.handshake_type() == tls_handshake_record_t::HANDSHAKE_TYPE_CLIENT_HELLO);
+        REQUIRE(hs.len() == 309);
+
+        REQUIRE(hs.payload()[0] == 3);
+        REQUIRE(hs.payload().size() == hs.len());
+        REQUIRE(hs.payload()[hs.payload().size() - 1] == 3);
+    }
+}
+
+TEST_CASE("Entities are parsed correctly together") {
+    SECTION("Handshake from Record is parsed correctly") {
+        std::ifstream ifs("../sample_data/tls_record_handshake_client_hello.bin", std::ifstream::binary);
+        kaitai::kstream ks(&ifs);
+        tls_record_t rec = tls_record_t(&ks);
+        std::string pl_copy(rec.payload());
+        kaitai::kstream hks(pl_copy);
+        tls_handshake_record_t hs = tls_handshake_record_t(&hks);
+
+        REQUIRE(hs.payload()[0] == 3);
+        REQUIRE(hs.payload().size() == hs.len());
+        REQUIRE(hs.payload()[hs.payload().size() - 1] == 3);
     }
 }
